77153154f073189d2e29b689f882e52e143fbdec
YujunLiao <18916108830@163.com>
2020-04-21 21:49:33 +0800

    Check point.

--- trainer.py
+++ trainer.py
@@ -105,17 +105,18 @@ class Trainer:
         self.cur_epoch = -1
         self.log_freq = 30
 
+        self.do_training()
+
 
     def _do_epoch(self):
         self.cur_epoch += 1
         self.scheduler.step()
         lrs = self.scheduler.get_lr()
-
-        criterion = nn.CrossEntropyLoss()
         p = OutputManager.print
+        criterion = nn.CrossEntropyLoss()
+
         # Set the mode of the model to trainer, then the parameters can begin to be trained
         self.model.train()
-
         # domain_index_of_images_in_this_patch is target domain index in the source domain list
         for i, ((data, rotation_label, class_label), domain_index_of_images_in_this_patch) in enumerate(self.train_data_loader):
             data, rotation_label, class_label, domain_index_of_images_in_this_patch = data.to(self.device), rotation_label.to(self.device), class_label.to(self.device), domain_index_of_images_in_this_patch.to(self.device)
@@ -141,51 +142,26 @@ class Trainer:
 
             del loss, supervised_task_loss, unsupervised_task_loss, rotation_predict_label, class_predict_label
 
-
+        # eval
         self.model.eval()
         with torch.no_grad():
             for phase, loader in self.test_loaders.items():
                 total = len(loader.dataset)
-                _, class_correct = self.do_test(loader)
+                class_correct = Trainer.eval(self.model, loader, device=self.device)
                 class_acc = float(class_correct) / total
                 p(f'{phase}_acc:c:{class_acc}')
                 self.results[phase][self.current_epoch] = class_acc
 
-
-    def do_test(self, loader):
-        jigsaw_correct = 0
+    @staticmethod
+    def eval(model, loader, device='cpu'):
         class_correct = 0
-        domain_correct = 0
-        for it, ((data, jig_l, class_l), _) in enumerate(loader):
-            data, jig_l, class_l = data.to(self.device), jig_l.to(self.device), class_l.to(self.device)
-            jigsaw_logit, class_logit = self.model(data)
+        for _, ((data, _, class_l), _) in enumerate(loader):
+            data, _, class_l = data.to(device), _.to(device), class_l.to(device)
+            _, class_logit = model(data)
             _, cls_pred = class_logit.max(dim=1)
-            _, jig_pred = jigsaw_logit.max(dim=1)
             class_correct += torch.sum(cls_pred == class_l.data)
-            jigsaw_correct += torch.sum(jig_pred == jig_l.data)
-        return jigsaw_correct, class_correct
+        return class_correct
 
-
-    def do_test_multi(self, loader):
-        jigsaw_correct = 0
-        class_correct = 0
-        single_correct = 0
-        for it, ((data, jig_l, class_l), d_idx) in enumerate(loader):
-            data, jig_l, class_l = data.to(self.device), jig_l.to(self.device), class_l.to(self.device)
-            n_permutations = data.shape[1]
-            class_logits = torch.zeros(n_permutations, data.shape[0], self.number_of_images_classes).to(self.device)
-            for k in range(n_permutations):
-                class_logits[k] = F.softmax(self.model(data[:, k])[1], dim=1)
-            class_logits[0] *= 4 * n_permutations  # bias more the original image
-            class_logit = class_logits.mean(0)
-            _, cls_pred = class_logit.max(dim=1)
-            jigsaw_logit, single_logit = self.model(data[:, 0])
-            _, jig_pred = jigsaw_logit.max(dim=1)
-            _, single_logit = single_logit.max(dim=1)
-            single_correct += torch.sum(single_logit == class_l.data)
-            class_correct += torch.sum(cls_pred == class_l.data)
-            jigsaw_correct += torch.sum(jig_pred == jig_l.data[:, 0])
-        return jigsaw_correct, class_correct, single_correct
 
     def do_training(self):
         start_time = time()
@@ -199,7 +175,6 @@ class Trainer:
         p(vars(self.args))
 
         # TODO(lyj):
-        self.logger = Logger(self.args, update_frequency=30)  # , "domain", "lambda"
         self.results = {"val": torch.zeros(self.args.epochs), "test": torch.zeros(self.args.epochs)}
         for self.current_epoch in range(self.args.epochs):
             self._do_epoch()
@@ -239,7 +214,142 @@ class Trainer:
             str("It took %g" % (time() - start_time))
         ])
 
-        return self.logger, self.model
+class BaseTrainer:
+    def __init__(self, args, model, data_loader, optimizer, scheduler, output_manager):
+        # self.args = args.args
+        self.args = args
+        self.device = args.device
+        self.model = model.to(args.device)
+        self.output_manager=output_manager
+
+        self.train_data_loader = data_loader.train_data_loader
+        self.validation_data_loader = data_loader.validation_data_loader
+        self.test_data_loader = data_loader.test_data_loader
+
+        self.optimizer = optimizer
+        self.scheduler = scheduler
+
+        self.classify_only_ordered_images_or_not = self.args.classify_only_ordered_images_or_not
+        self.number_of_images_classes = self.args.n_classes
+        self.test_loaders = {"val": self.validation_data_loader, "test": self.test_data_loader}
+
+        self.cur_epoch = -1
+        self.log_freq = 30
+
+        self.do_training()
+
+
+    def _do_epoch(self):
+        self.cur_epoch += 1
+        self.scheduler.step()
+        lrs = self.scheduler.get_lr()
+        p = OutputManager.print
+        criterion = nn.CrossEntropyLoss()
+
+        # Set the mode of the model to trainer, then the parameters can begin to be trained
+        self.model.train()
+        # domain_index_of_images_in_this_patch is target domain index in the source domain list
+        for i, ((data, rotation_label, class_label), domain_index_of_images_in_this_patch) in enumerate(self.train_data_loader):
+            data, rotation_label, class_label, domain_index_of_images_in_this_patch = data.to(self.device), rotation_label.to(self.device), class_label.to(self.device), domain_index_of_images_in_this_patch.to(self.device)
+            self.optimizer.zero_grad()
+
+            rotation_predict_label, class_predict_label = self.model(data)  # , lambda_val=lambda_val)
+            unsupervised_task_loss = criterion(rotation_predict_label, rotation_label)
+            supervised_task_loss = criterion(class_predict_label[rotation_label == 0], class_label[rotation_label == 0])
+
+            _, cls_pred = class_predict_label.max(dim=1)
+            _, jig_pred = rotation_predict_label.max(dim=1)
+            # _, domain_pred = domain_logit.max(dim=1)
+            loss = supervised_task_loss + unsupervised_task_loss * self.args.unsupervised_task_weight
+
+            loss.backward()
+            self.optimizer.step()
+            if i%self.log_freq == 0:
+                p([f'epoch:{self.cur_epoch}/{self.args.epochs};lr:{" ".join([str(lr) for lr in lrs])};'+\
+                   f'bs:{data.shape[0]};{i}/{len(self.train_data_loader)}',
+                   f'train_acc:j:{torch.sum(jig_pred == rotation_label.data).item()/data.shape[0]};'+\
+                      f'c:{torch.sum(cls_pred == class_label.data).item()/data.shape[0]}',
+                   f'train_loss:j:{unsupervised_task_loss.item()};c:{supervised_task_loss.item()}'])
+
+            del loss, supervised_task_loss, unsupervised_task_loss, rotation_predict_label, class_predict_label
+
+        # eval
+        self.model.eval()
+        with torch.no_grad():
+            for phase, loader in self.test_loaders.items():
+                total = len(loader.dataset)
+                class_correct = Trainer.eval(self.model, loader, device=self.device)
+                class_acc = float(class_correct) / total
+                p(f'{phase}_acc:c:{class_acc}')
+                self.results[phase][self.current_epoch] = class_acc
+
+    @staticmethod
+    def eval(model, loader, device='cpu'):
+        class_correct = 0
+        for _, ((data, _, class_l), _) in enumerate(loader):
+            data, _, class_l = data.to(device), _.to(device), class_l.to(device)
+            _, class_logit = model(data)
+            _, cls_pred = class_logit.max(dim=1)
+            class_correct += torch.sum(cls_pred == class_l.data)
+        return class_correct
+
+
+    def do_training(self):
+        start_time = time()
+        p = OutputManager.print
+        p('Start training')
+        p({
+            'train':self.train_data_loader.dataset,
+            'validation': self.validation_data_loader.dataset,
+            'test': self.test_data_loader.dataset,
+        })
+        p(vars(self.args))
+
+        # TODO(lyj):
+        self.results = {"val": torch.zeros(self.args.epochs), "test": torch.zeros(self.args.epochs)}
+        for self.current_epoch in range(self.args.epochs):
+            self._do_epoch()
+        val_res = self.results["val"]
+        test_res = self.results["test"]
+        idx_best = val_res.argmax()
+
+        # print("Best val %g, corresponding test %g - best test: %g" % (val_res.max(), test_res[idx_best], test_res.max()))
+        p(strftime("%Y-%m-%d %H:%M:%S", localtime()))
+        temp_dict = {
+            'source': self.args.source,
+            'target':self.args.target,
+            'param':self.args.parameters_lists,
+            'Highest accuracy on validation set appears on epoch': val_res.argmax().data,
+            'Highest accuracy on test set appears on epoch ': test_res.argmax().data,
+            'Accuracy on test set when the accuracy on validation set is highest:%.3f': test_res[idx_best],
+            'Highest accuracy on test set:%.3f': test_res.max(),
+
+        }
+        p(temp_dict)
+
+
+
+        self.output_manager.write_to_output_file([
+            '--------------------------------------------------------',
+            str(strftime("%Y-%m-%d %H:%M:%S", localtime())),
+            self.args.source,
+            "target domain:" + self.args.target,
+            "jigweight:" + str(self.args.unsupervised_task_weight),
+            "bias_hole_image:" + str(self.args.bias_whole_image),
+            "only_classify the ordered image:" + str(self.args.classify_only_ordered_images_or_not),
+            "batch_size:" + str(self.args.batch_size) + " learning_rate:" + str(self.args.learning_rate),
+            "Highest accuracy on validation set appears on epoch " + str(val_res.argmax().data),
+            "Highest accuracy on test set appears on epoch " + str(test_res.argmax().data),
+            str("Accuracy on test set when the accuracy on validation set is highest:%.3f" % test_res[idx_best]),
+            str("Highest accuracy on test set:%.3f" % test_res.max()),
+            str("It took %g" % (time() - start_time))
+        ])
+
+class DGTrainer(BaseTrainer):
+
+
+
+
 
 class Container:
     def __init__(self):
@@ -257,8 +367,7 @@ def lazy_train(args, output_manager):
     optimizer = get_optimizer(model, lr=args.learning_rate, train_all=args.train_all)
     scheduler = optim.lr_scheduler.StepLR(optimizer, int(args.epochs * .8))
     # scheduler = MyScheduler(temp, optimizer)
-    trainer = Trainer(args, model, data_loader, optimizer, scheduler, output_manager)
-    trainer.do_training()
+    Trainer(args, model, data_loader, optimizer, scheduler, output_manager)
 
 
 if __name__ == "__main__":
